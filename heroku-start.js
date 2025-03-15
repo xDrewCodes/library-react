88d1b32737f5cd30b48bf8b842ddf4c0e7b1a13b
Drew Allred <drew.e.allred@gmail.com>
2025-03-15 05:14:54 -0400

    fixed node server issues

--- heroku-start.js
+++ heroku-start.js
@@ -4,14 +4,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 // Your static pre-build assets folder
-app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 // Root Redirects to the pre-build assets
 app.get('/', function(req,res){
-  res.sendFile(path.join(__dirname, '..', 'build'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 // Any Page Redirects to the pre-build assets folder index.html that // will load the react app
 app.get('*', function(req,res){
-  res.sendFile(path.join(__dirname, '..', 'build/index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port, ()=>{
   console.log("Server is running on port: ", port)
